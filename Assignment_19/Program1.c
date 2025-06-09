7c960d6d16a8260b73eba63037b3589bb00da89b
Shrishti-Gaikwad <gaikwadshrishti04@gmail.com>
2025-06-10 00:32:32 +0530

    Add files via upload

---
+++ Assignment_19/Program1.c
@@ -0,0 +1,55 @@
+/* Accept N numbers from the user and return frequency of even numbers
+Input:  N:  6
+        Elements: 85 66 3 80 93 88
+Output: 3
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int CountEven(int Arr[],int iLength)
+{
+    int iCnt = 0;
+    int iEvenCount = 0;
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        if(Arr[iCnt] % 2 == 0)
+        {
+            iEvenCount++;
+        }
+    }
+    return iEvenCount;
+}
+
+int main()
+{
+    int iSize = 0,iCnt = 0,iRet = 0;
+    int *p = NULL;
+
+    printf("Enter the number of elements:");
+    scanf("%d",&iSize);
+
+    p = (int*)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d Elements\n",iSize);
+
+    for(iCnt = 0;iCnt < iSize;iCnt++)
+    {
+        printf("Enter Element %d:",iCnt + 1);
+        scanf("%d",&p[iCnt]);
+    }
+    iRet = CountEven(p,iSize);
+
+    printf("Result is : %d",iRet);
+
+    free(p);
+
+    return 0;
+}

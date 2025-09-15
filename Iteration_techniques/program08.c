b3b4c927476a0a7095f7a3c5913dd09449c888a6
Shrishti-Gaikwad <gaikwadshrishti04@gmail.com>
2025-09-15 22:31:54 +0530

    Added program based on iteration technique

---
+++ Iteration_techniques/program08.c
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////
+//
+//  File name : program08.c
+//  Description : Used to print numbers from user input using Iteration
+//  Author : Shrishti Sunil Gaikwad
+//  Date : 05/05/2025
+//
+////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int Calculate(int iValue)
+{
+    int iCnt = 0,iSum=0;
+    for(iCnt = 1  ;iCnt <= iValue ;iCnt++)
+    {
+        iSum = iSum + iCnt;
+    }
+    return iSum;
+
+}
+
+int main()
+{
+    int iNo = 0,iRet=0;
+    printf("Enter the Frequency:");
+    scanf("%d",&iNo);
+
+    iRet = Calculate(iNo);
+    printf("Sumation is : %d\n",iRet);
+
+    return 0;
+}

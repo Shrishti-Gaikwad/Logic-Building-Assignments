a6ed6e70d5c7d75aac72aac6767f7d0034415f4f
Shrishti-Gaikwad <gaikwadshrishti04@gmail.com>
2025-09-15 22:31:03 +0530

    Added program based on iteration technique

---
+++ Iteration_techniques/program02.c
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////
+//
+//  File name : program02.c
+//  Description : Used to print numbers from 1 to 4 using Iteration
+//  Author : Shrishti Sunil Gaikwad
+//  Date : 05/05/2025
+//
+////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void Display()
+{
+    int iCnt = 0;
+
+    for(iCnt = 1;iCnt <= 4;iCnt++)
+    {
+        printf("%d\n",iCnt);
+    }
+
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}

6e08f714db9b4630d3f03627a6750502d75e5a2d
Shrishti-Gaikwad <gaikwadshrishti04@gmail.com>
2025-09-16 20:19:32 +0530

    Added program based on itertaion

---
+++ Iteration_techniques/print_numbers_user_input01.c
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////
+//
+//  File name : print_numbers_user_input01.c
+//  Description : Used to print numbers from user input using Iteration
+//  Author : Shrishti Sunil Gaikwad
+//  Date : 05/05/2025
+//
+////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void Display(int iValue)
+{
+    int iCnt = 0;
+    for(iCnt=1;iCnt <= 5;iCnt++)
+    {
+        printf("%d\n",iValue);
+    }
+}
+
+int main()
+{
+    int iNo = 0;
+    printf("Enter the number:");
+    scanf("%d",&iNo);
+
+    Display(iNo);
+
+    return 0;
+}

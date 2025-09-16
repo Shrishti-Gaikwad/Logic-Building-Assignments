dbc5b1f348d135a94291dc30bba307ee1f71821a
Shrishti-Gaikwad <gaikwadshrishti04@gmail.com>
2025-09-16 20:18:11 +0530

    Added program based on itertaion

---
+++ Iteration_techniques/display_hello.c
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////
+//
+//  File name : display_hello.c
+//  Description : Used to print Hello 4times using Iteration
+//  Author : Shrishti Sunil Gaikwad
+//  Date : 05/05/2025
+//
+////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void Display()
+{
+    int iCnt = 0;
+
+    for(iCnt = 1;iCnt <= 4;iCnt++)
+    {
+        printf("Hello\n");
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}

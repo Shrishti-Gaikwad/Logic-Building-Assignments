8ec9f535733e72a5feef046cee5a805a05271478
Shrishti-Gaikwad <gaikwadshrishti04@gmail.com>
2025-11-19 02:24:56 +0530

    Add files via upload

---
+++ Assignment_49/Program1.java
@@ -0,0 +1,47 @@
+/* Write a java program which accepts string from user and display below pattern.
+Input :  Hello
+Output : H  *  *  *  *
+         H  e  *  *  *
+         H  e  l  *  *
+         H  e  l  l  *  
+*/
+
+import java.util.*;
+
+class Pattern
+{
+    public void Pattern(String str)
+    {
+        int i = 0,j = 0;
+
+        for(i = 0;i < str.length() - 1;i++)
+        {
+            for(j = 0;j < str.length();j++)
+            {
+                if(i >= j)
+                {
+                    System.out.print(str.charAt(j)+"\t");
+                }
+                else
+                {
+                    System.out.print("*\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Program1
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the String : ");
+        String str = sobj.nextLine();
+
+        Pattern pobj = new Pattern();
+        pobj.Pattern(str);
+    }
+}
---
+++ Assignment_49/Program2.java
@@ -0,0 +1,48 @@
+/* Write a java program which accepts string from user and display below pattern.
+Input :  Hello
+Output :  H  e  l  l  o
+          H  e  l  l  *
+          H  e  l  *  *
+          H  e  *  *  *
+          H  *  *  *  * 
+*/
+
+import java.util.*;
+
+class Pattern
+{
+    public void Pattern(String str)
+    {
+        int i = 0,j = 0;
+
+        for(i = 0;i < str.length();i++)
+        {
+            for(j = 0;j < str.length();j++)
+            {
+                if(j < str.length() - i)
+                {
+                    System.out.print(str.charAt(j)+"\t");
+                }
+                else
+                {
+                    System.out.print("*\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Program2
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the String : ");
+        String str = sobj.nextLine();
+
+        Pattern pobj = new Pattern();
+        pobj.Pattern(str);
+    }
+}

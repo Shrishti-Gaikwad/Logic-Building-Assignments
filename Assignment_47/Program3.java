c5e4ad76b162b2db0b304524ab55230a14bba77a
Shrishti-Gaikwad <gaikwadshrishti04@gmail.com>
2025-11-19 02:19:54 +0530

    Add files via upload

---
+++ Assignment_47/Program3.java
@@ -0,0 +1,45 @@
+/*
+Write java program which accept number of rows and number of columns from user and display below pattern.
+Input : iRow = 3, iCol = 5
+Output : 5      4       3       2       1
+         5      4       3       2       1
+         5      4       3       2       1
+*/          
+
+import java.util.*;
+
+class Pattern
+{
+    public void Pattern(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        for(i = iRow; i > 0; i--)
+        {
+            for(j = iCol; j > 0; j--)
+            {
+                System.out.print(j+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Program3
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter number of rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Pattern(iValue1, iValue2);
+    }
+}
